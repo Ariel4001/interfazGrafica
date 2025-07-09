90f93c8b44d52d0e645c5f5bc9be152669ed6455
daniela <luanacervantesemma@gmail.com>
2025-07-09 16:53:19 -0500

    lo que hicimos en clases

--- src/datos/personaDao.py
+++ src/datos/personaDao.py
@@ -6,11 +6,33 @@ class PersonaDao:
     _ERROR_ = -1
     _INSERT = ("INSERT INTO Persona (Nombre, apellido, cedula,"
                "Sexo, email) values (?, ?, ?, ?, ?)")
+    _SELECT = ('select nombre, apellido, cedula, sexo, email from Persona '
+               'where cedula = ?')
+    -_UPDATE = ('update persona set nombre = ?, apellido = ?, cedula = ?, sexo = ?, '
+                'email = ? where cedula = ?')
+    _DELETE = ('delete from Persona where cedula = ?')
 
 
 
     @classmethod
     def insertar_persona(cls, persona):
+        try:
+            #cursor = Conexion.obtenerCursor()
+            with Conexion.obtenerCursor() as cursor:
+                datos = (persona.nombre, persona.apellido, persona.cedula,
+                         persona.sexo, persona.email, persona.cedula,)
+                retorno = cursor.execute(cls._UPDATE, datos)
+
+
+                return retorno.rowcount
+
+        except Exception as e:
+            print(e)
+            cursor.rollback()
+            return cls._ERROR_
+
+    @classmethod
+    def actualizar_persona(cls, persona):
         try:
             #cursor = Conexion.obtenerCursor()
             with Conexion.obtenerCursor() as cursor:
@@ -28,9 +50,38 @@ class PersonaDao:
 
 
 
+
+
+
+    @classmethod
+    def seleccionar_persona(cls, cedula):
+        try:
+            #cursor = Conexion.obtenerCursor()
+            with Conexion.obtenerCursor() as cursor:
+                datos = (cedula,)
+                retorno = cursor.execute(cls._SELECT, datos).fetchone()
+                if retorno[3] == 'H':
+                    sexo = 'Hombre'
+                else:
+                    sexo = 'Mujer'
+                persona = Persona(nombre = retorno[0],
+                                 apellido = retorno[1],
+                                 cedula = retorno[2],
+                                 sexo = sexo,
+                                 email = retorno[4])
+                return persona
+
+        except Exception as e:
+            print(e)
+            cursor.rollback()
+            return None
+
+
 if __name__ == '__main__':
     p1 = Persona('Karen2', 'Carvo', '0123456789', 'F', 'arielandres779@gmail.com')
-    PersonaDao.insertar_persona(p1)
+    PersonaDao.actualizar_persona(p1)
+    # r1 = PersonaDao.seleccionar_persona('0123456789')
+    # print(r1)
 
 
 
--- src/servicio/Persona.py
+++ src/servicio/Persona.py
@@ -11,12 +11,15 @@ class PersonaServicio(QMainWindow):
         super(PersonaServicio, self).__init__()
         self.ui = Ui_vtnPrincipal()
         self.ui.setupUi(self)
-        self.ui.btnGuardar.clicked.connect(self.guardar)
-        self.ui.txtBorrar.clicked.connect(self.borrar)
+        self.ui.btnNuevo.clicked.connect(self.nuevo)
+        self.ui.btnLimpiar.clicked.connect(self.limpiar)
+        self.ui.btnBuscar.clicked.connect(self.buscar)
+        self.ui.btnActualizar.clicked.connect(self.actualizar)
         self.ui.txtCedula.setValidator(QIntValidator())
+        self.ui.txtBuscarCedula.setValidator(QIntValidator())
 
 
-    def guardar(self):
+    def nuevo (self):
         if (self.ui.txtNombre.text() == "" or self.ui.txtApellido.text() == "" or self.ui.txtCedula.text() == "" or len(self.ui.txtCedula.text()) < 10
                 or self.ui.cbSexo.currentText() == "Seleccionar" or self.ui.txtEmail.text() == ""):
             QMessageBox.warning(self, "Advertencia", "Completar datos")
@@ -36,18 +39,60 @@ class PersonaServicio(QMainWindow):
             #PersonaDao.insertar_persona(persona)
             else:
                 self.ui.statusbar.showMessage('Se guardo correctamente', 3000)
-                self.borrar()
+                self.limpiar()
 
 
 
 
 
-    def borrar(self):
+    def limpiar (self):
         self.ui.txtNombre.setText('')
         self.ui.txtApellido.setText('')
         self.ui.txtCedula.setText('')
         self.ui.txtEmail.setText('')
         self.ui.cbSexo.setCurrentText('seleccionar')
+
+
+
+
+
+    def buscar (self):
+        if len(self.ui.txtBuscarCedula.text()) < 10:
+            QMessageBox.warning(self, "Advertencia", "Ingrese la cedula a buscar.")
+
+        else:
+            persona = PersonaDao.seleccionar_persona(self.ui.txtBuscarCedula.text())
+            if persona:
+                self.ui.txtNombre.setText(persona.nombre)
+                self.ui.txtApellido.setText(persona.apellido)
+                self.ui.txtCedula.setText(persona.cedula)
+                self.ui.txtEmail.setText(persona.email)
+                self.ui.cbSexo.setCurrentText(persona.sexo)
+            else:
+               QMessageBox.warning(self, "Error", "No se encontro la persona con la cedula.")
+
+    def actualizar (self):
+        if (self.ui.txtNombre.text() == "" or self.ui.txtApellido.text() == "" or self.ui.txtCedula.text() == "" or len(self.ui.txtCedula.text()) < 10
+                or self.ui.cbSexo.currentText() == "Seleccionar" or self.ui.txtEmail.text() == ""):
+            QMessageBox.warning(self, "Advertencia", "Completar datos")
+
+        else:
+            sexo_completo = self.ui.cbSexo.currentText()
+            sexo_para_db = sexo_completo[0].upper()
+            persona = Persona(
+                nombre = self.ui.txtNombre.text(),
+                apellido = self.ui.txtApellido.text(),
+               cedula = self.ui.txtCedula.text(),
+                sexo = sexo_para_db,
+                email = self.ui.txtEmail.text())
+            #print(persona)
+            if PersonaDao.actualizar_persona(persona) == -1:
+                QMessageBox.critical(self, "Error", "No se pudo guardar.")
+            #PersonaDao.insertar_persona(persona)
+            else:
+                self.ui.statusbar.showMessage('Se actualizo correctamente', 3000)
+                self.limpiar()
+
 
 
 
